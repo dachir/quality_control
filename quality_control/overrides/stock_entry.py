7c768316c94391b14d101c0db2180696a4c239a4
dachir <dodziamouzou@gmail.com>
2025-01-09 13:08:27 +0000

    090120250944

--- quality_control/overrides/stock_entry.py
+++ quality_control/overrides/stock_entry.py
@@ -4,18 +4,18 @@ from erpnext.stock.doctype.stock_entry.stock_entry import StockEntry
 
 class CustomStockEntry(StockEntry):
         
-    def before_save(self):
+    def after_save(self):
         for i in self.items:             
             if self.stock_entry_type == "Material Transfer":
                 # Handle custom recontrol case first
                 if i.custom_recontrol == 1:
-                    i.to_quality_status = "Q"
+                    #i.to_quality_status = "Q"
                     frappe.db.set_value("Stock Entry Detail", i.name, "to_quality_status", "Q")
                 elif self.custom_control_quality == 0 and self.custom_status_change == 0:
                     if not i.quality_status:
-                        i.quality_status = "A"
+                        #i.quality_status = "A"
                         frappe.db.set_value("Stock Entry Detail", i.name, "quality_status", "A")
-                    i.to_quality_status = i.quality_status
+                    #i.to_quality_status = i.quality_status
                     frappe.db.set_value("Stock Entry Detail", i.name, "to_quality_status", i.quality_status)
 
             elif self.stock_entry_type == "Manufacture":
@@ -23,10 +23,10 @@ class CustomStockEntry(StockEntry):
                 if i.branch == "Kinshasa":
                     quality_control = frappe.db.get_value("Item", i.item_code, "custom_control_quality")
                     if i.t_warehouse and quality_control == 1:
-                        i.to_quality_status = "Q"
+                        #i.to_quality_status = "Q"
                         frappe.db.set_value("Stock Entry Detail", i.name, "to_quality_status", "Q")
                 else:
-                    i.to_quality_status = "A"
+                    #i.to_quality_status = "A"
                     frappe.db.set_value("Stock Entry Detail", i.name, "to_quality_status", "A")
 
 
